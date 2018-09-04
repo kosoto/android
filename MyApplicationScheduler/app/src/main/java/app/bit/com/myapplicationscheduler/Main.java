036527baca3920a0527b5c9348ca9a287aff7ee1
kosoto <kstad@naver.com>
2018-09-04 17:46:50 +0900

    180904

--- MyApplicationScheduler/app/src/main/java/app/bit/com/myapplicationscheduler/Main.java
+++ MyApplicationScheduler/app/src/main/java/app/bit/com/myapplicationscheduler/Main.java
@@ -1,22 +1,87 @@
 package app.bit.com.myapplicationscheduler;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main extends AppCompatActivity {
-
+    String date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         final Context ctx = Main.this;
-        findViewById(R.id.btnEnd).setOnClickListener(
+        TextView today = findViewById(R.id.today);
+        today.setText(new SimpleDateFormat("yyyy MM dd kk:mm:ss").format(new Date()));
+        CalendarView calender = findViewById(R.id.calender);
+        TimePicker time = findViewById(R.id.time);
+        TextView year = findViewById(R.id.year);
+        TextView month = findViewById(R.id.month);
+        TextView day = findViewById(R.id.day);
+        TextView hour = findViewById(R.id.hour);
+        TextView minute = findViewById(R.id.minute);
+        TextView second = findViewById(R.id.second);
+        calender.setVisibility(View.VISIBLE);
+        time.setVisibility(View.INVISIBLE);
+        findViewById(R.id.rdoCalendar).setOnClickListener(
                 (View v)->{
-                    Toast.makeText(ctx, "예약버튼 누름", Toast.LENGTH_SHORT).show();
+                    calender.setVisibility(View.VISIBLE);
+                    time.setVisibility(View.INVISIBLE);
+
                 }
         );
+
+        findViewById(R.id.rdoTime).setOnClickListener(
+                (View v)->{
+                    calender.setVisibility(View.INVISIBLE);
+                    time.setVisibility(View.VISIBLE);
+                }
+        );
+
+        findViewById(R.id.btnEnd).setOnClickListener(
+                (View v)->{
+                    date = String.format("%s %s %s %s:%s",
+                            year.getText().toString(),
+                            month.getText().toString(),
+                            day.getText().toString(),
+                            hour.getText().toString(),
+                            minute.getText().toString());
+                }
+        );
+        calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                TextView y = findViewById(R.id.year);
+                TextView m = findViewById(R.id.month);
+                TextView day = findViewById(R.id.day);
+                y.setText(year+"");
+                m.setText(month+"");
+                day.setText(dayOfMonth+"");
+
+            }
+        });
+        time.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(@NonNull TimePicker view, int hourOfDay, int minute) {
+                TextView h = findViewById(R.id.hour);
+                TextView m = findViewById(R.id.minute);
+                h.setText(hourOfDay+"");
+                m.setText(minute+"");
+            }
+        });
+
+
+
+
     }
 }
